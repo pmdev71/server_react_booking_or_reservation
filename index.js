6a6565a29b0809f0c554d58d9f3d6cf24e212432
Palash Mandal <pmpalashmandal@hmail.com>
2022-08-13 00:36:08 +0600

    connect with mongodb local server using mongoose

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected');
+});
+
+app.listen(8080, () => {
+  connect();
+  console.log('Server started on port 8080');
+});
